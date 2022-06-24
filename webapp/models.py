28ed9cc53f2c96e8213d327d241e2ede84a1350d
ErmekSolpuev <ermeksee27@gmail.com>
2022-06-24 15:49:38 +0600

    добавил вывод даты и изменил date_comletion в models

--- webapp/models.py
+++ webapp/models.py
@@ -7,7 +7,7 @@ class Sketchpad(models.Model):
     description = models.TextField(max_length=2000, null=False, blank=False, verbose_name="Описание")
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name="Статус")
     created_note = models.DateField(auto_now_add=True, verbose_name="Дата создания")
-    date_of_completion = models.DateField(auto_now=True, verbose_name="Дата выполнения")
+    date_of_completion = models.DateField(null=True, blank=True, verbose_name="Дата выполнения")
 
     def __str__(self):
         return f"{self.id}. {self.description}: {self.status} {self.date_of_completion}"
--- webapp/views.py
+++ webapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-from webapp.models import Sketchpad
+from webapp.models import Sketchpad, STATUS_CHOICES
 
 
 def index_view(request):
     sketchpads = Sketchpad.objects.order_by("date_of_completion")
     context = {"sketchpads": sketchpads}
     return render(request, "index.html", context)
+
 
 def sketchpad_view(request):
     pk = request.GET.get("pk")
@@ -17,11 +18,17 @@ def sketchpad_view(request):
 
 def create_sketchpad(request):
     if request.method == "GET":
-        return render(request, "create.html")
+        return render(request, "create.html", {"statuses": STATUS_CHOICES})
     else:
         description = request.POST.get("description")
         status = request.POST.get("status")
         date_of_completion = request.POST.get("date_of_completion")
-        new_sketchpad = Sketchpad.objects.create(description=description, status=status, date_of_completion=date_of_completion)
-        context = {"sketchpad": new_sketchpad}
-        return render(request, "sketchpad_view.html", context)
+        if date_of_completion == '':
+            date_of_completion = "2012-12-12"
+            new_sketchpad = Sketchpad.objects.create(description=description, status=status, date_of_completion=date_of_completion)
+            context = {"sketchpad": new_sketchpad}
+            return render(request, "sketchpad_view.html", context)
+        else:
+            new_sketchpad = Sketchpad.objects.create(description=description, status=status,date_of_completion=date_of_completion)
+            context = {"sketchpad": new_sketchpad}
+            return render(request, "sketchpad_view.html", context)
