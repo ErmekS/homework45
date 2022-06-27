3a8ad5f81afc8474a2d9c7438f1f05cbcf519f5b
ErmekSolpuev <ermeksee27@gmail.com>
2022-06-27 20:59:01 +0600

    добавил в models title

--- webapp/models.py
+++ webapp/models.py
@@ -8,6 +8,8 @@ class Sketchpad(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name="Статус")
     created_note = models.DateField(auto_now_add=True, verbose_name="Дата создания")
     date_of_completion = models.DateField(null=True, blank=True, verbose_name="Дата выполнения")
+    title = models.CharField(max_length=50, null=False, blank=False, default="No Title", verbose_name="Заголовок")
+
 
     def __str__(self):
         return f"{self.id}. {self.description}: {self.status} {self.date_of_completion}"
